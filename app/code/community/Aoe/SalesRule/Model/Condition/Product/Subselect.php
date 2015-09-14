9cc1666b8ba85b9f3906b63c84eebeef4a6a94ae
Lee Saferite <lee.saferite@aoe.com>
2015-09-14 15:59:27 +0000

    Extend the product found and subselection conditions to fix bugs

--- app/code/community/Aoe/SalesRule/Model/Condition/Product/Subselect.php
+++ app/code/community/Aoe/SalesRule/Model/Condition/Product/Subselect.php
@@ -2,5 +2,32 @@
 
 class Aoe_SalesRule_Model_Condition_Product_Subselect extends Mage_SalesRule_Model_Rule_Condition_Product_Subselect
 {
+    /**
+     * validate
+     *
+     * @param Varien_Object $object Quote
+     * @return boolean
+     */
+    public function validate(Varien_Object $object)
+    {
+        if (!$this->getConditions()) {
+            return false;
+        }
 
+        // Fix a bug with the underlying combine condition check
+        $value = $this->getValue();
+        $this->setValue(true);
+
+        $total = 0;
+        foreach ($object->getQuote()->getAllVisibleItems() as $item) {
+            if (parent::validate($item)) {
+                $total += $item->getData($this->getAttribute());
+            }
+        }
+
+        // Fix a bug with the underlying combine condition check
+        $this->setValue($value);
+
+        return $this->validateAttribute($total);
+    }
 }
