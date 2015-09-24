cca6f7faafe9fd3bba317bd5520ee8bc36c9bf87
Lee Saferite <lee.saferite@aoe.com>
2015-09-24 13:36:19 +0000

    Update how rule usage is recorded
    The observer recording rule usage was wrapped in a check for a zero discount and skipped registering rule usage in that case.
    Some third party rules result in zero amount discount but secondary results.
    The zero value check is pointless as if a rule is applied then it should be recorded in any case.
    
    Additionally, recording rule usage was subject to race conditions. Teh code read the current usage count then saved it back with the value incremented.
    Two customers triggering this code at the same time would result in a race since it's no an atomic operation.
    This has been replaced with atomic updates that uses DB expressions to increment the usage counters.

--- app/code/community/Aoe/SalesRule/Model/Observer.php
+++ app/code/community/Aoe/SalesRule/Model/Observer.php
@@ -1,6 +1,6 @@
 <?php
 
-class Aoe_SalesRule_Model_Observer
+class Aoe_SalesRule_Model_Observer extends Mage_SalesRule_Model_Observer
 {
     public function applyCoreSalesRule(Varien_Event_Observer $observer)
     {
@@ -68,6 +68,80 @@ class Aoe_SalesRule_Model_Observer
 
         if ($applied) {
             $result->setData('applied', true);
+        }
+    }
+
+    /**
+     * Observer used to record a rule usage and a coupon usage
+     *
+     * NB: This replaces the parent method so that event zero value discounts are recorded properly and to remove race conditions
+     *
+     * @param Varien_Event_Observer $observer
+     *
+     * @return void
+     */
+    public function sales_order_afterPlace(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Sales_Model_Order $order */
+        $order = $observer->getData('order');
+        if (!$order instanceof Mage_Sales_Model_Order) {
+            return;
+        }
+
+        // Get the used rule IDs
+        $ruleIds = array_unique(array_filter(array_map('intval', explode(',', $order->getAppliedRuleIds()))));
+
+        $ruleCustomer = null;
+        $customerId = $order->getCustomerId();
+
+        // use each rule (and apply to customer, if applicable)
+        foreach ($ruleIds as $ruleId) {
+            /** @var Mage_SalesRule_Model_Rule $rule */
+            $rule = Mage::getModel('salesrule/rule');
+            $rule->load($ruleId);
+            if (!$rule->getId()) {
+                continue;
+            }
+
+            // Update the rule usage counter - the DB expression is used to prevent race conditions
+            $rule->setDataUsingMethod('times_used', new Zend_Db_Expr('times_used+1'));
+            $rule->save();
+
+            if ($customerId) {
+                // If we add a unique index of rule and customer we can use \Varien_Db_Adapter_Interface::insertOnDuplicate to prevent race conditions
+                /** @var Mage_SalesRule_Model_Rule_Customer $ruleCustomer */
+                $ruleCustomer = Mage::getModel('salesrule/rule_customer');
+                $ruleCustomer->loadByCustomerRule($customerId, $ruleId);
+
+                if ($ruleCustomer->getId()) {
+                    // Update the usage counter - the DB expression is used to prevent race conditions
+                    $ruleCustomer->setDataUsingMethod('times_used', new Zend_Db_Expr('times_used+1'));
+                } else {
+                    $ruleCustomer
+                        ->setCustomerId($customerId)
+                        ->setRuleId($ruleId)
+                        ->setTimesUsed(1);
+                }
+
+                $ruleCustomer->save();
+            }
+        }
+
+        // Trim the coupon code to match all the other coupon code processing
+        $couponCode = trim($order->getCouponCode());
+        if (!empty($couponCode)) {
+            /** @var Mage_SalesRule_Model_Coupon $coupon */
+            $coupon = Mage::getModel('salesrule/coupon');
+            $coupon->load($couponCode, 'code');
+            if ($coupon->getId()) {
+                $coupon->setDataUsingMethod('times_used', new Zend_Db_Expr('times_used+1'));
+                $coupon->save();
+                if ($customerId) {
+                    /** @var Aoe_SalesRule_Model_Resource_Coupon_Usage $couponUsage */
+                    $couponUsage = Mage::getResourceModel('salesrule/coupon_usage');
+                    $couponUsage->updateCustomerCouponTimesUsed($customerId, $coupon->getId());
+                }
+            }
         }
     }
 }
---
+++ app/code/community/Aoe/SalesRule/Model/Resource/Coupon/Usage.php
@@ -0,0 +1,45 @@
+<?php
+
+class Aoe_SalesRule_Model_Resource_Coupon_Usage extends Mage_SalesRule_Model_Resource_Coupon_Usage
+{
+    /**
+     * Increment times_used counter
+     *
+     * NB: This replaces the parent method to fix a possible race condition on usage tracking.
+     *
+     * @param int $customerId
+     * @param int $couponId
+     */
+    public function updateCustomerCouponTimesUsed($customerId, $couponId)
+    {
+        $read = $this->_getReadAdapter();
+        $select = $read->select();
+        $select->from($this->getMainTable(), array('times_used'))
+            ->where('coupon_id = :coupon_id')
+            ->where('customer_id = :customer_id');
+
+        $timesUsed = $read->fetchOne($select, array(':coupon_id' => $couponId, ':customer_id' => $customerId));
+
+        if ($timesUsed > 0) {
+            $this->_getWriteAdapter()->update(
+                $this->getMainTable(),
+                array(
+                    'times_used' => new Zend_Db_Expr('times_used+1'),
+                ),
+                array(
+                    'coupon_id = ?'   => $couponId,
+                    'customer_id = ?' => $customerId,
+                )
+            );
+        } else {
+            $this->_getWriteAdapter()->insert(
+                $this->getMainTable(),
+                array(
+                    'coupon_id'   => $couponId,
+                    'customer_id' => $customerId,
+                    'times_used'  => 1,
+                )
+            );
+        }
+    }
+}
